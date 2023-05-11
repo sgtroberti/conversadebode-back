b15b274c3970554f38f05d76f627aad93550fa27
Guilherme Roberti <guimourinha@gmail.com>
2023-05-11 16:56:12 -0300

    fix

--- src/database/index.js
+++ src/database/index.js
@@ -1,8 +1,13 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb://localhost/conversadebode");
+mongoose.connect(
+  "mongodb+srv://robertiTest:robertiTest@cluster0.xanxa.mongodb.net/vidracaria?retryWrites=true&w=majority"
+);
 mongoose.Promise = global.Promise;
 
 export default mongoose;
 
 // "mongodb+srv://robertiTest:robertiTest@cluster0.xanxa.mongodb.net/vidracaria?retryWrites=true&w=majority"
+//mongodb+srv://root:<password>@gabriel.83b9tsg.mongodb.net/?retryWrites=true&w=majority
+
+//"mongodb://localhost/conversadebode"
